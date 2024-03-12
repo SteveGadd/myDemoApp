c513a319a3ee6cab9134734450c2f55cbd4a296b
vinniecolaiutacs <janagircan@hotmail.com>
2024-03-12 09:39:08 +0300

    Appropriate changes made in App.java and mustache template

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -36,26 +36,40 @@ public class App
           //System.out.println(req.queryParams("input1"));
           //System.out.println(req.queryParams("input2"));
 
-          String input1 = req.queryParams("input1");
-          java.util.Scanner sc1 = new java.util.Scanner(input1);
-          sc1.useDelimiter("[;\r\n]+");
-          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
-          while (sc1.hasNext())
-          {
+        String input1 = req.queryParams("input1");
+        java.util.Scanner sc1 = new java.util.Scanner(input1);
+        sc1.useDelimiter("[;\r\n]+");
+        java.util.ArrayList<Integer> inputList1 = new java.util.ArrayList<>();
+        while (sc1.hasNext()){
             int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
-            inputList.add(value);
-          }
-          System.out.println(inputList);
+            inputList1.add(value);
+        }
+        System.out.println(inputList1);
+
+        String input2 = req.queryParams("input2");
+        java.util.Scanner sc2 = new java.util.Scanner(input2);
+        sc2.useDelimiter("[;\r\n]+");
+        java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
+        while (sc2.hasNext()){
+            int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+            inputList2.add(value);
+        }
+        System.out.println(inputList2);
 
 
-          String input2 = req.queryParams("input2").replaceAll("\\s","");
-          int input2AsInt = Integer.parseInt(input2);
+        String input3 = req.queryParams("input3").replaceAll("\\s","");
+        int index1 = Integer.parseInt(input3);
 
-          int  result = App.isSumOfRangeEqual(inputList);
+        String input4 = req.queryParams("input4").replaceAll("\\s","");
+        int index2 = Integer.parseInt(input4);
 
-         Map map = new HashMap();
-          map.put("result", result);
-          return new ModelAndView(map, "compute.mustache");
+        boolean result = App.isSumOfRangeEqual(inputList1, inputList2,index1,index2);
+
+        Map map = new HashMap();
+        map.put("result", result);
+        sc1.close();
+        sc2.close();
+        return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
 
 
@@ -73,6 +87,6 @@ public class App
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-
+        return -1;
     }
 }
